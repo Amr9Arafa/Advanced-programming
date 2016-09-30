f30fddd54b96717aa012b6c0c0148185a0c6cd95
Amr Mohamed <amr.mohamed.mahmoud.aly@gmail.com>
2016-09-30 19:20:29 +0200

    added console program

---
+++ Week2/Console_lab_2/Console_lab_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorldProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+
+            string str = Console.ReadLine();
+            Console.WriteLine(str);
+
+            int i = 50;
+
+            Console.WriteLine("the Variable is equal to ={0}", i);
+
+            Console.ReadLine();
+        }
+    }
+}
