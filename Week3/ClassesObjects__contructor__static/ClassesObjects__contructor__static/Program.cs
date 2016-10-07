55209ffd36c7c4e22e7f990c0c9a7d7b882dc101
Amr Mohamed <amr.mohamed.mahmoud.aly@gmail.com>
2016-10-07 17:50:50 +0200

    week3 example on classes,objects,contructor and static

---
+++ Week3/ClassesObjects__contructor__static/ClassesObjects__contructor__static/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1.name = "Amr";
+            s1.age = 24; ;
+            s1.id = 10102102;
+
+            s1.register();
+           int numberOfHours= s1.numberOfFinishedhours(5);
+           int studentId = s1.getStudentId();
+           string studentName = s1.getStudentName();
+
+           Console.WriteLine("the student whose name is {0} and his id is {1} has finished {2} hours",studentName,studentId,numberOfHours);
+
+           Console.ReadLine();
+        }
+    }
+}
---
+++ Week3/ClassesObjects__contructor__static/ClassesObjects__contructor__static/student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Student
+    {
+
+        public String name;
+        public int age;
+        public int id;
+
+        public void register()
+        {
+            Console.WriteLine("{0} has registered successfully",name);
+        }
+
+        public int numberOfFinishedhours(int term)
+        {
+            return term * 18;
+        }
+
+        public int getStudentAge()
+        {
+            return age;
+        }
+
+        public int getStudentId()
+        {
+            return id;
+        }
+        public String getStudentName()
+        {
+            return name;
+        }
+
+    }
+}
