feb59ab9efbd5df2be9ba231acbfee48e4746073
Amr Mohamed <amr.mohamed.mahmoud.aly@gmail.com>
2016-10-29 00:30:31 +0200

    Week6

---
+++ Week6/Overloading/Overloading/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Overloading
+{
+    class Product
+    {
+        public int Year;
+        public string Name;
+        public decimal price;
+
+
+
+        public Product(string s)
+        {
+            Name = s;
+        }
+        public Product(string s, int y)
+        {
+            Name = s;
+            Year = y;
+        }
+        public Product(string s, decimal p, int y)
+        {
+            Name = s;
+            price = p;
+            Year = y;
+        }
+
+        public void clean(string s1)
+        {
+            Console.WriteLine("This is clean method with only 1 parameter");
+        }
+
+        public void clean(string s1, string s2)
+        {
+            Console.WriteLine("This is clean method with only 2 parameter");
+        }
+        public void clean(string s1 ,string s2 ,string s3)
+        {
+            Console.WriteLine("This is clean method with only 3 parameter");
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product("Shampoo");
+            Product p2 = new Product("Deodrant", 2004);
+            Product p3 = new Product("Soap", 120.00m, 1994);
+
+            Console.WriteLine("{0}", p1.Name);
+            Console.WriteLine("{0} {1}", p2.Year, p2.Name);
+            Console.WriteLine("{0} {1} {2}", p3.Year, p3.Name, p3.price);
+
+
+            p1.clean("");
+            p1.clean("","");
+            p1.clean("","","");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Week6/week3-Abstract/week3-Abstract/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace week3_Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Animal
+    {
+
+
+        public void eat()
+        {
+            Console.WriteLine(" eating ");
+        }
+
+        public void sleep(int hours)
+        {
+            Console.WriteLine("Sleeping");
+        }
+
+        public void Walk()
+        {
+            Console.WriteLine("walking");
+        }
+
+
+
+        public abstract void makeNoise();
+   
+    }
+
+
+ 
+       class Dog : Animal
+        {
+
+        public void makeNoise()
+        {
+            Console.WriteLine("BARK BARK !!");
+        }
+        
+
+        }
+
+       class Cow : Animal
+       {
+
+           public void makeNoise()
+           {
+               Console.WriteLine("MOW MOW !!");
+           }
+
+
+       }
+
+    class cat :Animal
+    {
+
+        public override void  makeNoise()
+        {
+ 	        Console.WriteLine("MEOW MEOW !!");
+        }
+
+    }
+
+
+
+
+}
