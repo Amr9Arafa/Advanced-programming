8d83c84113c16b7152c37761b60f568914c8f728
Amr Mohamed <amr.mohamed.mahmoud.aly@gmail.com>
2016-12-17 00:40:40 +0200

    weeek 13 added

---
+++ Week13/passbyrefrence/passbyrefrence/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace passbyrefrence
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    namespace NamedOptionalParams
+    {
+        class Program
+        {
+            
+            static void Main(string[] args){
+
+                int x = 10;
+
+                myFunction(ref x);
+
+                Console.WriteLine("{0}",x);
+
+                Console.ReadLine();
+           
+            }
+
+             static void myFunction( ref int  y){
+
+                 y += 10;
+            
+              
+            }
+
+           
+
+
+        }
+
+
+    }
+}
